b845a5eadf3f03e03d7a54919dedd18d410642a6
Evan Brecht-Curry <starfoxz13@gmail.com>
2019-12-02 15:40:12 -0800

    added styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,68 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import './main.css'
 
-function Canvas() {
+const box = 25;
+let sort;
+let arr = [];
+let arrLength = 0;
+let currentStep = 0;
+let savedArray = [];
+let changedIndex = [];
+let highlightedIndex = 0;
+let sortingSpeed = 200;
+const audio = new Audio('./assets/click.mp3');
+
+export default function Canvas() {
   const canvasRef = React.useRef(null);
 
   React.useEffect(() => {
-
     generateArray(48);
+  });
 
+  function bubbleSort() {
+    let sorted = false;
+    let sorts = 0;
+    savedArray = [];
+
+    while(!sorted) {
+      sorts = 0;
+      for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+          let temp = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = temp;
+
+          // making array immutable
+          let arr2 = arr.slice();
+
+          savedArray.push(arr2);
+          changedIndex.push(i);
+          sorts ++;
+        }
+      }
+      if (sorts === 0) {
+        sorted = true;
+      }
+    }
+    return changedIndex
+  }
+
+  // generates array to be sorted
+  function generateArray (arrLength) {
+    arr = [];
+    for (let i = 0; i < arrLength; i++) {
+      arr.push(i + 1);
+    }
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    bubbleSort();
+    handleDrawing();
+  }
+
+  function handleDrawing() {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, 1200, 625);
@@ -26,44 +82,27 @@ function Canvas() {
       }
       ctx.fillRect(box / 2 + (box * i), box * 25, box / 2, (-box / 2) * arr[i]);
     }
-  });
+  }
 
 
   return (
+<>
+  <div id='controlPanel'>
+    <Button variant="outline-success">Auto Sort</Button>
+    <Button variant="outline-danger">Stop Auto Sort</Button>
+    <Button variant="outline-warning">Previous Step</Button>
+    <Button variant="outline-info">Next Step</Button>
+    <Button variant="outline-secondary">Reset</Button>
+  </div>
     <canvas
       ref={canvasRef}
       width={1200}
       height={625}
     />
+</>
   )
 }
 
 
-const box = 25;
-let sort;
-let arr = [];
-let arrLength = 0;
-let currentStep = 0;
-let savedArray = [];
-let changedIndex = [];
-let highlightedIndex = 0;
-let sortingSpeed = 200;
-const audio = new Audio('./assets/click.mp3');
-
-// generates array to be sorted
-function generateArray (arrLength) {
-  arr = [];
-  for (let i = 0; i < arrLength; i++) {
-    arr.push(i + 1);
-  }
-  for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]];
-  }
-  console.log(arr);
-  // bubbleSort();
-}
 
 
-
-export default Canvas
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Canvas from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(<Canvas />, document.getElementById('root'));
